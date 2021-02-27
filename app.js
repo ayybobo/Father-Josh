f72399f17e40902c6db8f57ffc398c831332986d
ayybobo <71397971+ayybobo@users.noreply.github.com>
2021-02-27 01:27:22 -0600

    Initial Commit

---
+++ app.js
@@ -0,0 +1,403 @@
+const menu = [
+  {
+    id: 1,
+    title: "create your own",
+    category: "pizza",
+    img: "/images/cyo-slate-compressed.jpg"
+  },
+  {
+    id: 2,
+    title: "pepperoni",
+    category: "pizza",
+    img: "/images/pepperoni-slate-compressed.jpg"
+  },
+  {
+    id: 3,
+    title: "sausage",
+    category: "pizza",
+    img: "/images/sausage-slate-compressed.jpg"
+  },
+  {
+    id: 4,
+    title: "cheese pizza",
+    category: "pizza",
+    img: "/images/cheese-slate-compressed.jpg"
+  },
+  {
+    id: 5,
+    title: "the works",
+    category: "pizza",
+    img: "/images/the-works-slate-compressed.jpg"
+  },
+  {
+    id: 6,
+    title: "the meats",
+    category: "pizza",
+    img: "/images/the-meats-slate-compressed.jpg"
+  },
+  {
+    id: 7,
+    title: "meatball pepperoni",
+    category: "pizza",
+    img: "/images/meatball-pepp-slate-compressed.jpg"
+  },
+  {
+    id: 8,
+    title: "super hawaiian",
+    category: "pizza",
+    img: "/images/super-hawaiian-slate-compressed.jpg"
+  },
+  {
+    id: 9,
+    title: "extra cheesy alfredo",
+    category: "pizza",
+    img: "/images/extra-cheesy-alfredo-slate-compressed.jpg"
+  },
+  {
+    id: 10,
+    title: "garden fresh",
+    category: "pizza",
+    img: "/images/garden-fresh-slate-compressed.jpg"
+  },
+  {
+    id: 11,
+    title: "tuscan six cheese",
+    category: "pizza",
+    img: "/images/tuscan-6cheese-slate-compressed.jpg"
+  },
+  {
+    id: 12,
+    title: "hawaiian BBQ chicken",
+    category: "pizza",
+    img: "/images/hawaiian-bbq-slate-compressed.jpg"
+  },
+  {
+    id: 13,
+    title: "ultimate pepperoni",
+    category: "pizza",
+    img: "/images/ultimate-pepp-slate-compressed.jpg"
+  },
+  {
+    id: 14,
+    title: "zesty italian trio",
+    category: "pizza",
+    img: "/images/zesty-italian-trio-slate-compressed.jpg"
+  },
+  {
+    id: 15,
+    title: "fiery buffalo chicken",
+    category: "pizza",
+    img: "/images/fiery-buffalo-chicken-slate-compressed.jpg"
+  },
+  {
+    id: 16,
+    title: "pepperoni, sausage & six cheese",
+    category: "pizza",
+    img: "/images/pepp-sausage-6cheese-slate-compressed.jpg"
+  },
+  {
+    id: 17,
+    title: "BBQ chicken bacon",
+    category: "pizza",
+    img: "/images/bbq-chicken-bacon-slate-compressed.jpg"
+  },
+  {
+    id: 18,
+    title: "grilled buffalo chicken",
+    category: "papadias",
+    img: "/images/buffalo-chicken-papadia-compressed.jpg"
+  },
+  {
+    id: 19,
+    title: "italian papadia",
+    category: "papadias",
+    img: "/images/italian-papadia-compressed.jpg"
+  },
+  {
+    id: 20,
+    title: "BBQ chicken bacon papadia",
+    category: "papadias",
+    img: "/images/BBQ-chicken-bacon-papadia-compressed.jpg"
+  },
+  {
+    id: 21,
+    title: "philly cheesesteak papadia",
+    category: "papadias",
+    img: "/images/philly-steak-papadia-compressed.jpg"
+  },
+  {
+    id: 22,
+    title: "meatball pepperoni papadia",
+    category: "papadias",
+    img: "/images/meatball-pepp-papadia-compressed.jpg"
+  },
+  {
+    id: 23,
+    title: "jalapeno popper rolls",
+    category: "sides",
+    img: "/images/jalapeno-poppers-compressed.jpg"
+  },
+  {
+    id: 24,
+    title: "garlic knots",
+    category: "sides",
+    img: "/images/garlic-knots-slate-compressed.jpg"
+  },
+  {
+    id: 25,
+    title: "garlic parmesan breadsticks",
+    category: "sides",
+    img: "/images/garlic-parm-breadsticks-slate-compressed.jpg"
+  },
+  {
+    id: 26,
+    title: "cheesesticks",
+    category: "sides",
+    img: "/images/cheese-sticks-slate-compressed.jpg"
+  },
+  {
+    id: 27,
+    title: "original breadsticks",
+    category: "sides",
+    img: "/images/original-breadsticks-slate-compressed.jpg"
+  },
+  {
+    id: 28,
+    title: "unsauced roasted wings",
+    category: "sides",
+    img: "/images/unsauced-roasted-slate-compressed.jpg"
+  },
+  {
+    id: 29,
+    title: "buffalo wings",
+    category: "sides",
+    img: "/images/new-buffalo-wings-slate-compressed.jpg"
+  },
+  {
+    id: 30,
+    title: "BBQ wings",
+    category: "sides",
+    img: "/images/new-bbq-wings-slate-compressed.jpg"
+  },
+  {
+    id: 31,
+    title: "honey chipotle wings",
+    category: "sides",
+    img: "/images/new-honey-chip-wings-slate-compressed.jpg"
+  },
+  {
+    id: 32,
+    title: "chicken poppers",
+    category: "sides",
+    img: "/images/single-chix-nugget-compressed.jpg"
+  },
+  {
+    id: 33,
+    title: "cinnamon pull aparts",
+    category: "desserts",
+    img: "/images/cinn-pullaparts-slate-compressed.jpg"
+  },
+  {
+    id: 34,
+    title: "chocolate chip cookie",
+    category: "desserts",
+    img: "/images/cookie-slice-compressed.jpg"
+  },
+  {
+    id: 35,
+    title: "double chocolate chip brownie",
+    category: "desserts",
+    img: "/images/brownie-slate-compressed.jpg"
+  },
+  {
+    id: 36,
+    title: "pepsi",
+    category: "drinks",
+    img: "/images/std_Pepsi-compressed.webp"
+  },
+  {
+    id: 37,
+    title: "mountain dew",
+    category: "drinks",
+    img: "/images/2ltr-20oz-mtn-dew-compressed.webp"
+  },
+  {
+    id: 38,
+    title: "diet mountain dew",
+    category: "drinks",
+    img: "/images/2ltr-20oz-diet-mtn-dew-compressed.webp"
+  },
+  {
+    id: 39,
+    title: "diet pepsi",
+    category: "drinks",
+    img: "/images/std_Dt_Pepsi-compressed.webp"
+  },
+  {
+    id: 40,
+    title: "pepsi zero sugar",
+    category: "drinks",
+    img: "/images/pepsi-zero-sugar-compressed.webp"
+  },
+  {
+    id: 41,
+    title: "aquafina",
+    category: "drinks",
+    img: "/images/std_aquafina-compressed.webp"
+  },
+  {
+    id: 42,
+    title: "sierra mist",
+    category: "drinks",
+    img: "/images/Sierra-Mist-2liter-20oz-compressed.webp"
+  },
+  {
+    id: 43,
+    title: "special garlic sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-garlic-compressed.jpg"
+  },
+  {
+    id: 44,
+    title: "pizza suace",
+    category: "extras",
+    img: "/images/dipping-sauce-pizza-nocover-compressed.jpg"
+  },
+  {
+    id: 45,
+    title: "ranch sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-ranch-compressed.jpg"
+  },
+  {
+    id: 46,
+    title: "blue cheese sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-bluecheese-compressed.jpg"
+  },
+  {
+    id: 47,
+    title: "buffalo sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-buffalo-compressed.jpg"
+  },
+  {
+    id: 48,
+    title: "BBQ sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-bbq-compressed.jpg"
+  },
+  {
+    id: 49,
+    title: "honey mustard sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-honey-mustard-compressed.jpg"
+  },
+  {
+    id: 50,
+    title: "cheese sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-cheese-compressed.jpg"
+  },
+  {
+    id: 51,
+    title: "honey chipotle sauce",
+    category: "extras",
+    img: "/images/dipping-sauce-honey-chipotle-compressed.jpg"
+  },
+  {
+    id: 52,
+    title: "special seasonings",
+    category: "extras",
+    img: "/images/season-packet-spec-seasonings-compressed.jpg"
+  },
+  {
+    id: 53,
+    title: "crushed red pepper",
+    category: "extras",
+    img: "/images/season-packet-red-pepper-compressed.jpg"
+  },
+  {
+    id: 54,
+    title: "parmesan cheese",
+    category: "extras",
+    img: "/images/season-packet-parm-cheese-compressed.jpg"
+  },
+  {
+    id: 55,
+    title: "pepperoncinis",
+    category: "extras",
+    img: "/images/pepperoncinis-slate-compressed.jpg"
+  },
+]
+
+const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
+
+//load items
+window.addEventListener('DOMContentLoaded', () => {
+  displayMenuItems(menu);
+});
+
+//filter items
+filterBtns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter((menuItem) => {
+      //console.log(menuItem.category)
+    if(menuItem.category === category) {
+      return menuItem;
+    }
+    });
+    //console.log(menuCategory)
+  if(category === "all"){
+    displayMenuItems(menu);
+  } else {
+        displayMenuItems(menuCategory)
+      }
+  })
+})
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map((item) => {
+      return `<article class="menu-item">
+                <img src=${item.img} class="photo" alt=${item.title}>
+                <div class="item-info">
+                  <h4 class="item-name">${item.title}</h4>
+                  <button class="order-button">Order Now</button>
+                </div>
+              </article>`;
+  });
+    displayMenu = displayMenu.join('')
+    sectionCenter.innerHTML = displayMenu;
+}
+
+
+//MOBILE NAVBAR
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav1 = document.querySelector('.nav-links1');
+  const navLinks1 = document.querySelectorAll('.nav-links1 li');
+  
+  burger.addEventListener('click', () => {
+    //Toggle Nav
+    nav1.classList.toggle('nav-active');
+
+  // Animate Links Dynamically
+  navLinks1.forEach((link, index) => {
+    if(link.style.animation) {
+      link.style.animation = ''
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 10 + 0.4}s`;
+      }
+    });
+    //Burger Animation
+  burger.classList.toggle('toggle')
+  }); 
+};
+// All Functions
+const app = () => {
+  navSlide();
+};
+// Envoking All Functions
+app();
